99c696b2ea73429780cd77b22e8982130de18dc3
pavel <pavelmail07@gmail.com>
2017-06-19 12:45:30 +0300

    optimized page.php

--- wp-content/themes/eshopper/page.php
+++ wp-content/themes/eshopper/page.php
@@ -2,7 +2,6 @@
 <section>
     <div class="container woocommerce">
         <div class="row">
-            <?php if(!is_cart() && !is_checkout()){get_sidebar('left');}?>
             <div class="col-sm-9 padding-right">
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                     <?php the_content(); ?>
